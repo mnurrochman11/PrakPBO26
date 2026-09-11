b31694c5682d016876c4adb0831b6cd4f6239958
mnurrochman11 <mnurrochman1103@gmail.com>
2026-09-11 09:47:56 +0700

    jobsheet3_percobaan1

---
+++ Jobsheet3/src/Motor.java
@@ -0,0 +1,13 @@
+public class Motor {
+    public int kecepatan = 0;
+    public boolean kontakon = false;
+
+    public void printStatus() {
+        if (kontakon == true) {
+            System.out.println("Kontak On");
+        } else {
+            System.out.println("Kontak Off");
+        }
+        System.out.println("Kecepatan " + kecepatan + "\n");
+    }
+}
