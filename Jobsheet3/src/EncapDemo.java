63f163f8a69735a9cc75a60e1c3edc87e2aeeb6f
mnurrochman11 <mnurrochman1103@gmail.com>
2026-09-13 18:40:20 +0700

    jobsheet3_tugas3

--- Jobsheet3/src/EncapDemo.java
+++ Jobsheet3/src/EncapDemo.java
@@ -14,11 +14,21 @@ public class EncapDemo {
         return age;
     }
 
+    // public void setAge(int newAge) {
+    //     if (newAge > 30) {
+    //         age = 30;
+    //     } else {
+    //         age = newAge;
+    //     }
+    // }
+
     public void setAge(int newAge) {
-        if (newAge > 30) {
-            age = 30;
-        } else {
-            age = newAge;
-        }
+    if (newAge > 30) {
+        age = 30;
+    } else if (newAge < 18) {
+        age = 18;
+    } else {
+        age = newAge;
     }
+}
 }
