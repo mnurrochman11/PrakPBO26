5024d2629b11fb4533775138c0f403b0bd535114
mnurrochman11 <mnurrochman1103@gmail.com>
2026-09-13 18:42:57 +0700

    jobsheet3_tugas5

--- Jobsheet3/src/Kontainer.java
+++ Jobsheet3/src/Kontainer.java
@@ -35,11 +35,21 @@ public class Kontainer {
         }
     }
 
+    // public void turunkanMuatan(double berat) {
+    //     if (beratMuatanSaatIni - berat < 0) {
+    //         System.out.println("Maaf, muatan yang diturunkan melebihi muatan saat ini.");
+    //     } else {
+    //         beratMuatanSaatIni -= berat;
+    //     }
+    // }
+
     public void turunkanMuatan(double berat) {
-        if (beratMuatanSaatIni - berat < 0) {
-            System.out.println("Maaf, muatan yang diturunkan melebihi muatan saat ini.");
-        } else {
-            beratMuatanSaatIni -= berat;
-        }
+    if (berat > (0.5 * beratMuatanSaatIni)) {
+        System.out.println("Maaf, demi keselamatan, pembongkaran muatan satu kali jalan tidak boleh melebihi 50% dari muatan saat ini!");
+    } else if (beratMuatanSaatIni - berat < 0) {
+        System.out.println("Maaf, muatan yang diturunkan melebihi muatan saat ini.");
+    } else {
+        beratMuatanSaatIni -= berat;
     }
+}
 }
